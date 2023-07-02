c9ffd82f66ea2b87c43ba54f29acb7ffdbbf97ed
anujababy1 <email@example.com>
2023-07-02 19:41:46 -0400

    Refactoring the component to enhance its reusability.

--- vue/src/pages/Dashboard.vue
+++ vue/src/pages/Dashboard.vue
@@ -1,19 +1,12 @@
 <template>
 
  <div>
-     <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-      </div>
-    </main>
+    <page-component title="Dashboard">
+      my dashboard content
+    </page-component>
  </div>
 </template>
 
-<script setup>
-
+<script>
+ import PageComponent from '../components/PageComponent.vue'
 </script>
