7f0205d778b91a80646107a3b57cd16d87173df1
anujababy1 <email@example.com>
2023-07-03 19:34:29 -0400

    Register fontend implimentation

--- vue/src/pages/Register.vue
+++ vue/src/pages/Register.vue
@@ -1,7 +1,3 @@
-<script>
-
-</script>
-
 <template>
 <div>
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,25 +6,33 @@
    </div>
 
    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" @submit="register">
+        <div v-if="error" class="error-register">{{error}}</div>
           <div>
           <label for="fullname" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
           <div class="mt-2">
-            <input id="fullname" name="fullname" type="text" autocomplete="fullname" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="fullname" v-model="user.name" name="fullname" type="text" autocomplete="fullname" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
         
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="email" v-model="user.email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password" v-model="user.password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+            <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">Password Confirmation</label>
+          <div class="mt-2">
+            <input id="password_confirmation" v-model="user.password_confirmation" name="password_confirmation" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -46,7 +50,40 @@
 </div>
 </template>
 
+<script>
+  export default{
+    data(){
+      return {
+        user:{
+          name:null,
+          email:null, 
+          password:null,
+          password_confirmation:null
+        },
+        error:null
+      }
+    },
+    methods:{
+       async register(event){
+        event.preventDefault();
+        //console.log(this.user);
+        //this.$store.dispatch('register',this.user).then(() => {this.$router.push({ name: 'Dashboard'})});
+         try {
+        
+          await this.$store.dispatch('register', this.user);
+          this.$router.push({ name: 'Dashboard'});
+      } catch (err) {
+        this.error = err.message || 'Failed to authenticate, try later.';
+      }
+      }
+    }
+  }
+</script>
+
+
 
 <style scoped>
-
+  .error-register{
+    color:red;
+  }
 </style>
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -4,12 +4,12 @@ const store = createStore({
         return {
             user:{
                 data:{
-                    name: 'Tom Cook',
-                    email: 'tom@example.com',
-                    imageUrl:
-                      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    // name: 'Tom Cook',
+                    // email: 'tom@example.com',
+                    // imageUrl:
+                    //   'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 },
-                token:123,
+                token:localStorage.getItem('token'),
             }
         }
     },
@@ -20,10 +20,51 @@ const store = createStore({
         logout(state){
             state.user.data ={};
             state.user.token =null;
+        },
+        setUser(state,obj){
+            state.user.data = obj.user;
+            state.user.token = obj.token;
+            localStorage.setItem('token', obj.token);
         }
     },
     actions: { 
+        async register(context,user){
+        //     return fetch(`http://127.0.0.1:8000/api/register`, {
+        //     headers: {
+        //     'Content-Type': 'application/json',
+        //     'Accept': 'application/json'
+        //     },
+        //     method: 'POST',
+        //     body: JSON.stringify(user)
+        // }) .then(res => res.json())
+        //     .then(res => {
+        //         //console.log(res);
+        //         // if (!res.ok) {
+        //         //     alert();
+        //         // }
+        //         context.commit('setUser', res);
+        //         return res;
+        //     })
 
+        const response = await fetch('http://127.0.0.1:8000/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(user)
+          });
+      
+          const res = await response.json(); 
+          if (!response.ok) {
+            const error = new Error(
+              res.message || 'Failed to authenticate. Check your login data.'
+            );
+            throw error;
+          }
+            context.commit('setUser', res);
+          
+        }
     },
     
   });
